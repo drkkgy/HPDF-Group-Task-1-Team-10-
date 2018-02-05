8fb9838a6e59d402e98412e7bd9f9ace71045718
Manish Sharma <31022778+manishgrd@users.noreply.github.com>
2018-02-05 22:10:54 +0530

    signup page
    Register / Signup page by Anandita

---
+++ notify_frontend_web/Updated/register/src/AppBarExample.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar'
+
+const AppBarExampleIcon = () => (
+  <AppBar
+    title="Login Page"
+    titleStyle={{textAlign:'left', fontWeight:'bold'}}
+    showMenuIconButton={false}
+  />
+);
+
+export default AppBarExampleIcon;
