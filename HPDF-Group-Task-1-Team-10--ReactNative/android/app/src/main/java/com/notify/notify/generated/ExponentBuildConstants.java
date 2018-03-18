56a4dcc3044381bc16efc8f1802b2d289a395fa0
Manish Sharma <manish160490@gmail.com>
2018-03-18 19:23:18 +0530

    Updated Screenshots & Removed buggy ReactNative code

--- HPDF-Group-Task-1-Team-10--ReactNative/android/app/src/main/java/com/notify/notify/generated/ExponentBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package com.notify.notify.generated;
-
-// This file is auto-generated. Please don't rename!
-public class ExponentBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "exp3f3a214e63ad48ffaef9e0bcd4e06836://localhost:80";
-
-}
